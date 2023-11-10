bfe210ac5e2de5dce5c8c16e41789e1122bfa915
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-11-10 19:39:43 +0530

    basic styling with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"contact",component:ContactComponent},
   {path:"**",component:ErrorPageComponent}
 ];
 
